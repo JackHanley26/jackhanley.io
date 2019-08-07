97cbb68db640d859802764ece3f3cb88d377c8bd
jackhanley <jack.hanley@mycit.ie>
2019-08-07 12:17:47 -0400

    Added tech stack section (not filled out)

--- config/main.js
+++ config/main.js
@@ -10,7 +10,224 @@ export const home = {
   name: "Jack Hanley",
   title: "I build things with code.",
   summary: "I'm a software engineer from Ireland based in New York, NY",
-  aboutMe: "Hey! I'm Jack, a full-stack software engineer with over 3 years experience working on several proof of concept research projects and products in IBM.",
+  aboutMe: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
 };
 
 export const email = "jackhanley26@hotmail.com";
+/*
+
+    {
+      name: "",
+      label: "",
+      link: "",
+    },
+
+ */
+
+export const techStack = {
+  languages: [
+    {
+      name: "javascript",
+      label: "JavaScript (ES6+)",
+      link: "https://www.javascript.com/",
+    },
+    {
+      name: "node",
+      label: "NodeJS",
+      link: "https://nodejs.org/en/about/"
+    },
+    {
+      name: "python",
+      label: "Python2/3",
+      link: "https://www.python.org/about/"
+    },
+    {
+      name: "java",
+      label: "Java 8+",
+      link: "https://go.java/index.html"
+    },
+    {
+      name: "go",
+      label: "GoLang (Learning)",
+      link: "https://golang.org/"
+    },
+    {
+      name: "html",
+      label: "HTML",
+      link: "",
+    },
+    {
+      name: "css",
+      label: "CSS",
+      link: "",
+    },
+    {
+      name: "sass",
+      label: "SASS",
+      link: "",
+    },
+    {
+      name: "",
+      label: "",
+      link: "",
+    },
+  ],
+  frameworks: [
+    {
+      name: "react",
+      label: "ReactJS",
+      link: "https://reactjs.org/"
+    },
+    {
+      name: "reduxAndSagas",
+      label: "Redux & Sagas",
+      link: "https://redux-saga.js.org/"
+    },
+    {
+      name: "angular",
+      label: "AngularJS",
+      link: "https://angularjs.org/"
+    },
+    {
+      name: "express",
+      label: "ExpressJS",
+      link: "https://expressjs.com/"
+    },
+    {
+      name: "flask",
+      label: "Flask.py",
+      link: "https://flask.palletsprojects.com"
+    },
+    {
+      name: "spring",
+      label: "Spring",
+      link: "https://spring.io/"
+    },
+    {
+      name: "tensorflow",
+      label: "TensorFlow",
+      link: "https://www.tensorflow.org/"
+    },
+    {
+      name: "",
+      label: "",
+      link: ""
+    },
+
+  ],
+  libraries: [
+    {
+      name: "gsap",
+      label: "GSAP",
+      link: "https://greensock.com/gsap/"
+    },
+    {
+      name: "bulma",
+      label: "Bulma",
+      link: "https://bulma.io/"
+    },
+    {
+      name: "materialdesign",
+      label: "Material Design",
+      link: "https://material.io/"
+    },
+    {
+      name: "echarts",
+      label: "E-Charts",
+      link: "https://echarts.apache.org"
+    },
+    {
+      name: "chartjs",
+      label: "ChartJS",
+      link: "http://www.chartjs.org/"
+    },
+    {
+      name: "d3",
+      label: "D3",
+      link: "https://d3js.org/"
+    },
+    {
+      name: "lodash",
+      label: "Lodash",
+      link: "https://lodash.com/"
+    },
+    {
+      name: "",
+      label: "",
+      link: ""
+    },
+    {
+      name: "",
+      label: "",
+      link: ""
+    },
+    {
+      name: "keras",
+      label: "Keras",
+      link: "https://keras.io/"
+    },
+  ],
+  other:[
+    {
+      name: "cloudant",
+      label: "Cloudant (NoSQL)",
+      link: ""
+    },
+    {
+      name: "mysql",
+      label: "MySQL",
+      link: ""
+    },
+    {
+      name: "sqlite",
+      label: "SQLite",
+      link: ""
+    },
+    {
+      name: "cassandra",
+      label: "Cassandra",
+      link: ""
+    },
+    {
+      name: "arango",
+      label: "ArangoDB (AQL)",
+      link: ""
+    },
+    {
+      name: "postgres",
+      label: "Postgres",
+      link: ""
+    },
+    {
+      name: "redis",
+      label: "Redis",
+      link: ""
+    },
+    {
+      name: "rest",
+      label: "REST APIs",
+      link: ""
+    },
+    {
+      name: "sockets",
+      label: "Sockets",
+      link: ""
+    },
+    {
+      name: "kafka",
+      label: "Kafka",
+      link: ""
+    },
+    {
+      name: "mqtt",
+      label: "MQTT",
+      link: "https://mqtt.org"
+    },
+    {
+      name: "",
+      label: "",
+      link: ""
+    },
+
+  ]
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Layout from "../components/Layout/Layout";
-import {home, email} from "../config/main";
+import {home, email, techStack} from "../config/main";
 import {TimelineLite} from "gsap";
 import Button from "../components/Button/Button";
 import Divider from "../components/Divider/Divider";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCode, faTools, faBook, faTerminal, faCaretRight} from "@fortawesome/free-solid-svg-icons";
 
 class Index extends Component {
 
@@ -20,6 +22,7 @@ class Index extends Component {
 
   componentDidMount() {
     this.myTween
+      .delay(0.5)
       .from(this.greeting, 0.7, {y: 30, opacity: 0})
       .from(this.title, 0.3, {y: 30, opacity: 0})
       .from(this.subtitle, 0.3, {y: 30, opacity: 0})
@@ -28,8 +31,12 @@ class Index extends Component {
       .play();
   }
 
-  openMail() {
+  static openMail() {
     window.location.href = `mailto:${email}`;
+  }
+
+  static openLinkInTab(url){
+    window.open(url,'_blank');
   }
 
   render() {
@@ -41,16 +48,14 @@ class Index extends Component {
 
             <h1 className="primary-font-color code-font" ref={r => this.greeting = r}>{home.greeting}</h1>
 
-            <h1 className="title-text secondary-font-color title-text" ref={r => this.title = r}>{home.name}</h1>
+            <h1 className="title-text bright-font-color title-text" ref={r => this.title = r}>{home.name}</h1>
 
             <h1 className="title-text dull-font-color" ref={r => this.subtitle = r}>{home.title}</h1>
 
-
             <p className="default-text dull-font-color" ref={r => this.summary = r}>{home.summary}</p>
 
-
-            <div ref={r => this.button = r} style={{'margin-top': '3rem'}}>
-              <Button label="Say hello :)" onClick={this.openMail}/>
+            <div className="button-wrapper" ref={r => this.button = r}>
+              <Button label="Get in touch." onClick={Index.openMail}/>
             </div>
 
           </div>
@@ -66,33 +71,93 @@ class Index extends Component {
                 <p className="format-text dull-font-color">{home.aboutMe}</p>
               </div>
               <div className="column is-centered">
-                <figure className="image is-128x128 is-centered" ref={r => this.profile = r}>
+                <figure className="image is-128x128 is-horizontal-center" ref={r => this.profile = r}>
                   {/*<img className="is-rounded" src="../static/profile.png"/>*/}
-                  <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"/>
+                  <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"
+                       alt="Profile Picture"/>
                 </figure>
               </div>
             </div>
           </div>
         </section>
 
-        {/*        <section className="section hero is-primary">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                Primary title
-              </h1>
-              <h2 className="subtitle">
-                Primary subtitle
-              </h2>
+        <section id="technologies" className="section">
+          <div className="container">
+            <Divider label={'Tech stack'}/>
+
+            <div className="columns">
+
+              <div className="column">
+                <div className="bright-font-color">
+                  <FontAwesomeIcon icon={faCode}/>
+                  <span className="tech-title">Languages</span>
+                  <div className="tech-list dull-font-color">
+                    <ul>
+                      {techStack.languages.map(item => (
+                        <li>
+                          <span className="code-font primary-font-color">#</span>
+                          <a className="tech-item code-font" href={item.link} target="_blank">{item.label}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="bright-font-color">
+                  <FontAwesomeIcon icon={faTools}/>
+                  <span className="tech-title">Frameworks</span>
+                  <div className="tech-list dull-font-color">
+                    <ul>
+                      {techStack.frameworks.map(item => (
+                        <li>
+                          <span className="code-font primary-font-color">#</span>
+                          <a className="tech-item code-font" href={item.link} target="_blank">{item.label}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className="column">
+                <div className="bright-font-color">
+                  <FontAwesomeIcon icon={faBook}/>
+                  <span className="tech-title">Libraries</span>
+                  <div className="tech-list dull-font-color">
+                    <ul>
+                      {techStack.libraries.map(item => (
+                        <li>
+                          <span className="code-font primary-font-color">#</span>
+                          <a className="tech-item code-font" href={item.link} target="_blank">{item.label}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className="column">
+                <div className="bright-font-color">
+                  <FontAwesomeIcon icon={faTerminal}/>
+                  <span className="tech-title">Other</span>
+                  <div className="tech-list dull-font-color">
+                    <ul>
+                      {techStack.other.map(item => (
+                        <li>
+                          <span className="code-font primary-font-color">#</span>
+                          <a className="tech-item code-font" href={item.link} target="_blank">{item.label}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
             </div>
+
           </div>
         </section>
-
-        <section className="section">
-          <div className="">
-
-          </div>
-        </section>*/}
 
       </Layout>
     )
